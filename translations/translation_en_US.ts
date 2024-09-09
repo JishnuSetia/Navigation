ff5334ff816caf86e31eed325fce0fe0890cbf8c
Jishnu Setia <jishnusetia8@gmail.com>
2024-09-09 13:04:02 +0400

    Commit

---
+++ translations/translation_en_US.ts
@@ -0,0 +1,193 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello First Graders! May I come in?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello First Graders! May I come in?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Pepper</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Pepper</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I am Pepper, your new friend from GEMS Modern Academy</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I am Pepper, your new friend from GEMS Modern Academy</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (10)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation>Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (11)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Thank you so much!! I'm loving it...pararapapa</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Thank you so much!! I'm loving it...pararapapa</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (12)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Oh yes! I was hoping you would ask me that. I will be hanging around. Do stop by and take time to interact whenever you spot me.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Oh yes! I was hoping you would ask me that. I will be hanging around. Do stop by and take time to interact whenever you spot me.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (13)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I have a prepared a performance for you…for that you need to sit back and enjoy!. Till next time..have a great day!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I have a prepared a performance for you…for that you need to sit back and enjoy!. Till next time..have a great day!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Nice to meet you and your class. I came from the Digital Learning Centre.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Nice to meet you and your class. I came from the Digital Learning Centre.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (3)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Can you guess where I am from?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Can you guess where I am from?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (4)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Sure..Here you go</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Sure..Here you go</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (5)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>That's right! I would like your students to guide me to the School Discovery Center?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">That's right! I would like your students to guide me to the School Discovery Center?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (6)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Move Fail</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Move Fail</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (7)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Move Fail</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Move Fail</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (8)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Move Fail</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Move Fail</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (9)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Move Fail</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Move Fail</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Timeline/behavior_layer1/keyframe1/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+</TS>
